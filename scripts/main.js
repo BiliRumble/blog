e0fd4f29c922f81cd5677371d0dce75331602861
BiliRumble <bilirumble@outlook.com>
2024-11-15 20:00:25 +0800

    feat: 更新hexo

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+const { patch_config } = require('./patch_config');
+
+const main = () => {
+    patch_config();
+}
+
+main();
---
+++ scripts/patch_config.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const yaml = require('js-yaml');
+
+const patch = () => {
+    try {
+        const doc = yaml.safeLoad(fs.readFileSync('_config.butterfly.yml', 'utf8'));
+        doc.gitalk.client_secret = process.env.GITALK_CLIENT_SECRET;
+        fs.writeFileSync('_config.butterfly.yml', yaml.safeDump(doc), 'utf8');
+        console.debug('Config patched successfully.');
+    } catch (e) {
+        console.error(`Failed to patch config: ${e.message}`)
+    }
+}
+
+module.exports = {
+    patch
+}
