c7f1f67176a34c248375c4eaee4ddbd005177110
BiliRumble <bilirumble@outlook.com>
2024-11-16 16:51:45 +0800

    feat: 自定义主题

---
+++ source/assets/js/index.js
@@ -0,0 +1,17 @@
+const loadUtils = () => new Promise((resolve, reject) => {
+    let tag = document.createElement("script");
+    tag.src = "/assets/js/util.js";
+    tag.onload = () => resolve();
+    tag.onerror = () => reject();
+    document.head.appendChild(tag);
+});
+
+const init = async () => {
+    try {
+        await loadExternalResource("/assets/js/live2d-widget/autoload.js", "js");
+    } catch (e) {console.error(e)} finally {
+        initLive2d();
+    }
+}
+
+loadUtils().then(init);
