eba646ea96f2ad0e16cc36964e9d318d56b7be4d
BiliRumble <bilirumble@outlook.com>
2024-11-24 00:54:44 +0800

    fea: 更新主题

--- themes/mora/scripts/events/init.js
+++ themes/mora/scripts/events/init.js
@@ -5,15 +5,15 @@ hexo.extend.filter.register('before_generate', () => {
 
   if (hexoVer < 5.3) {
     log.error('Please update Hexo to V5.3.0 or higher!')
-    log.error('請把 Hexo 升級到 V5.3.0 或更高的版本！')
+    log.error('请升级 Hexo 至 V5.3.0+！')
     process.exit(-1)
   }
 
   if (locals.get) {
     const data = locals.get('data')
     if (data && data.butterfly) {
-      log.error("'butterfly.yml' is deprecated. Please use '_config.butterfly.yml'")
-      log.error("'butterfly.yml' 已經棄用，請使用 '_config.butterfly.yml'")
+      log.error("'butterfly.yml' is deprecated. Please use '_config.mora.yml'")
+      log.error("'butterfly.yml' 已弃用，请使用 '_config.mora.yml'")
       process.exit(-1)
     }
   }
